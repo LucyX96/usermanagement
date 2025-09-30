33bc224dc054437a72aa8287707362d742a4993e
Luciano Bellusi <luciano.bellusi@gmail.com>
2025-09-30 14:05:55 +0200

    first commit configuration

--- src/main/java/com/company/usermanagement/UsermanagementApplication.java
+++ src/main/java/com/company/usermanagement/UsermanagementApplication.java
@@ -4,9 +4,11 @@ import com.company.usermanagement.configuration.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties.class)
+@PropertySource("classpath:.env")
 public class UsermanagementApplication {
 
 	public static void main(String[] args) {
